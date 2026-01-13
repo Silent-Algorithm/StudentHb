7da89901afce36da6a8390a43700b9f2ab81a019
Gama Tuh Micheal Glenn <tohmichael62@gmail.com>
2026-01-13 21:58:54 +0100

    my final mvp

--- student/index.js
+++ student/index.js
@@ -301,7 +301,7 @@ const studData =
         function logout() {
             if (confirm('Are you sure you want to log out?')) {
                 // Redirect to login page
-                window.location.href = 'index.html';
+                window.location.href = '/index.html';
             }
         }
 
